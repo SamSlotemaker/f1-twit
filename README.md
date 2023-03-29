68af70ec03b22c3cf16255dceeab759494b3703b
Sam Slotemaker <sam.slotemaker@wearetriple.com>
2023-03-29 17:14:41 +0200

    add: fake login with redirect

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # F1-Twit client (WIP)
 
+App is testable but won't load tweets without the server.
+
 ![Imgur](https://imgur.com/6PEmYfW.png)
 
 ### General idea
@@ -9,8 +11,11 @@ Twitter clone where users can express their thoughts about Formula 1. Data will 
 
 ## Checklist client
 
+- [x] static login page
 - [x] homepage
+- [x] favorites page
 - [x] react routes
 - [x] loading state
+- [x] errpr state
 - [x] responsive menu
-- [ ] fetch from backend
+- [x] fetch from backend
--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.scss";
 
 const loginPage = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const navigate = useNavigate();
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    console.log("redirect");
+    navigate("/");
+  };
+
   return (
     <main className="login-page">
       <div className="login-page_background-image"></div>
 
-      <form className="login-page_form">
+      <form className="login-page_form" onSubmit={handleLogin}>
         <h1 className="login-page_form_welcome-header">Welcome.</h1>
         <label htmlFor="username">Username</label>
         <input type="text" name="username" id="username" />
