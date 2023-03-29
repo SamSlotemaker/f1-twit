c297a2da71f28b6fb3a932ab90ab5db5aa22e76e
Sam Slotemaker <sam.slotemaker@wearetriple.com>
2023-03-29 15:36:22 +0200

    add: fetching some data from the backend

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -5,14 +5,31 @@ import Page from "../page-wrapper";
 
 const HomePage = () => {
   const newChirpPlaceholder = "What are your thoughts about the Ford deal?";
+  const ENDPOINT = "http://localhost:3500";
 
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`${ENDPOINT}/api/tweets`);
+      const dataJSON = await response.json();
+      setData(dataJSON);
+    } catch (e) {
+      setError(true);
+    }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
+
+    getData();
   }, []);
+
+  console.log(data);
 
   return (
     <Page>
